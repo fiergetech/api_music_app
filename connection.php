ea9f0b6f373f0914e3017327f7309b6e9e8e1c3e
fiergetech <hindrowahid@gmail.com>
2023-03-31 10:32:48 +0700

    aoi_music_app

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$serverHost = "localhost";
+$user = "root";
+$password = "";
+$database = "mr_music_app_db";
+
+$connectNow =  new mysqli($serverHost, $user, $password, $database);
---
+++ user/signup.php
@@ -0,0 +1,19 @@
+<?php
+include '../connection.php';
+
+$userName =  $_POST ['user_name'];
+$userEmail =  $_POST ['user_email'];
+$userPassword =  md5($_POST ['user_password']);
+$userBalance = ['user_balance'];
+$userCoin = ['user_coin'];
+
+$sqlQuery = "INSERT INTO users_table SET user_name = '$userName', user_email = '$userEmail', user_password = '$userPassword', user_balance = '$userBalance', user_coin = '$userCoin'";
+
+$resultOfQuery =  $connectNow->query($sqlQuery);
+
+if($resultOfQuery){
+    echo json_encode(array("success"=>true));
+}
+else{
+    echo json_encode(array("success"=>false));    
+}
