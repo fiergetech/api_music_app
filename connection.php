79d1b748645c4726de97d2c3be91f4282e93d8c4
fiergetech <hindrowahid@gmail.com>
2023-04-02 12:13:06 +0700

    api validasi fix

--- connection.php
+++ connection.php
@@ -3,5 +3,6 @@ $serverHost = "localhost";
 $user = "root";
 $password = "";
 $database = "mr_music_app_db";
+$table = "users_table";
 
 $connectNow =  new mysqli($serverHost, $user, $password, $database);
